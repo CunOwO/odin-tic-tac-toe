4ac46c05991d18658564dc1dd410ee3f188c6f8d
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-12-31 15:30:58 +0700

    feat: Add core functionality for Tic Tac Toe game mechanics
    - Implement `Gameboard` module to initialize a 3x3 board with `Cell` objects.
      - Includes `getBoard`, `placeMarker`, and `printBoard` methods.
    - Create `Cell` module to represent individual board cells.
      - Provides `addMarker` and `getValue` methods for cell interaction.
    - Develop `GameController` to manage player turns and game flow.
      - Supports player switching, marker placement, and board display.
      - Includes methods to play rounds and track active player.

---
+++ script.js
@@ -0,0 +1,89 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;  
+
+    const placeMarker = (row, column, player) => {
+        if (board.getValue != 0) {
+            return;
+        }
+
+        board[row][column].addMarker(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, placeMarker, printBoard };
+}
+
+function Cell() {
+    let value = "";
+
+    const addMarker = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMarker,
+        getValue
+    };
+}
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: "O"
+        },
+        {
+            name: playerTwoName,
+            marker: "X"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`Marking ${getActivePlayer().name}'s marker into row ${row}, column ${column}...`);
+        board.addMarker(row, column, getActivePlayer().marker);
+
+        // Check winner and win message
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer,
+        getBoard: board.getBoard
+    };
+}
